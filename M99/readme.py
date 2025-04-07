170ec164bfa24b89e54761aee6ca3e7af7909902
EnriqueIborra <47239270+EnriqueIborra@users.noreply.github.com>
2025-04-07 12:59:25 +0200

    Create readme.py

---
+++ M99/readme.py
@@ -0,0 +1,2 @@
+# Python tiene una cantidad de curiosidades y/o diferencias con otros lenguajes por las que he dedicado un documento aparte para su exposición.
+# Admás, python es un lenguaje en constante evolucion, por lo que no es de extrañar que sigan apareciendo mas funcionalidades de este tipo.
