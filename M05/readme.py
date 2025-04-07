64a7d3b955e96b15603b4fb421cbeda374790134
EnriqueIborra <47239270+EnriqueIborra@users.noreply.github.com>
2025-04-07 11:46:49 +0200

    Create readme.py

---
+++ M05/readme.py
@@ -0,0 +1,2 @@
+# La esteganografía es el arte y la ciencia de ocultar un mensaje dentro de otro mensaje o medio de forma que no se perciba su presencia. 
+# A diferencia de la criptografía, que cifra los mensajes para que solo ciertas personas puedan entenderlos (pero sigue siendo evidente que existe un mensaje), la esteganografía tiene como objetivo ocultar la existencia misma del mensaje
