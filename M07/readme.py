70e99d862140a122b0cf20a8e831a395e1409a97
EnriqueIborra <47239270+EnriqueIborra@users.noreply.github.com>
2025-04-07 11:49:21 +0200

    Create readme.py

---
+++ M07/readme.py
@@ -0,0 +1,2 @@
+# El estándar X.509 es uno de los más utilizados para la creación y validación de certificados digitales, que proporcionan autenticación e integridad en las comunicaciones en línea. 
+# X.509 define el formato para los certificados que contienen información sobre la identidad de un sujeto (una persona, organización, etc.) y su clave pública, y está basado en un sistema de clave pública
