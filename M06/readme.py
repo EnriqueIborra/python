160d73dac927b46fc72450fa263e3cb6c4958da7
EnriqueIborra <47239270+EnriqueIborra@users.noreply.github.com>
2025-07-31 11:35:49 +0200

    Update readme.py

--- M06/readme.py
+++ M06/readme.py
@@ -1,3 +1,1 @@
-# La criptografía simétrica es un enfoque en el cual tanto el emisor como el receptor utilizan la misma clave secreta para cifrar y descifrar los datos. 
-# Es uno de los métodos más utilizados debido a su eficiencia y rapidez. 
-# Sin embargo, uno de los principales desafíos es la gestión segura de la clave, ya que si la clave es comprometida, toda la comunicación se ve vulnerada
+# 
